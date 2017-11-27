4c5f36621f1bd558c2ed8810d327551261824acf
Julio <dr.rodriguez-garcia@protonmail.com>
2017-11-27 10:06:09 +0000

    Added spec files for compilation (windows spec missing)

--- simcav_main.py
+++ simcav_main.py
@@ -51,6 +51,18 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
+# File path function for deployment in single file with PyInstaller
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
 #==============================================================================
 #%% Toolbar
 class Toolbar(tk.Frame):
